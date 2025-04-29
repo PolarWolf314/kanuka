b600e2c93d58bf375fee9f28d86b4214dccf8b88
Aaron Guo <89121695+PolarWolf314@users.noreply.github.com>
2025-04-29 18:45:57 +1200

    feat: create crypto functions

--- internal/secrets/crypto.go
+++ internal/secrets/crypto.go
@@ -1,1 +1,26 @@
 package secrets
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"os"
+)
+
+func LoadPrivateKey(path string) (*rsa.PrivateKey, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	block, _ := pem.Decode(data)
+	if block == nil || block.Type != "RSA PRIVATE KEY" {
+		return nil, fmt.Errorf("failed to decode PEM block containing private key")
+	}
+	return x509.ParsePKCS1PrivateKey(block.Bytes)
+}
+
+func DecryptWithPrivateKey(ciphertext []byte, privateKey *rsa.PrivateKey) ([]byte, error) {
+	return rsa.DecryptPKCS1v15(rand.Reader, privateKey, ciphertext)
+}
