d7bc0ad5804f70832cd0a5aec7ae7f5706dbf70b
Aaron Guo <89121695+PolarWolf314@users.noreply.github.com>
2025-04-30 21:44:41 +1200

    feat: add decrypt files method

--- internal/secrets/crypto.go
+++ internal/secrets/crypto.go
@@ -10,6 +10,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"golang.org/x/crypto/nacl/secretbox"
 )
@@ -243,5 +244,40 @@ func EncryptFiles(symKey []byte, inputPaths []string) error {
 	log.Println("✅ All environment files in the project have been encrypted 🎉")
 	log.Printf("The following files were written: %s", FormatPaths(outputPaths))
 
+	return nil
+}
+
+func DecryptFiles(symKey []byte, inputPaths []string) error {
+	if len(symKey) != 32 {
+		return fmt.Errorf("failed as symmetric key must be 32 bytes for secretbox")
+	}
+	var key [32]byte
+	copy(key[:], symKey)
+	var outputPaths []string
+	for _, inputPath := range inputPaths {
+		ciphertext, err := os.ReadFile(inputPath)
+		if err != nil {
+			return fmt.Errorf("failed to read .kanuka file at %s: %w", inputPath, err)
+		}
+
+		// Extract the nonce from the beginning of the ciphertext
+		var decryptNonce [24]byte
+		copy(decryptNonce[:], ciphertext[:24])
+
+		// Decrypt using the extracted nonce and the rest of the ciphertext
+		plaintext, ok := secretbox.Open(nil, ciphertext[24:], &decryptNonce, &key)
+		if !ok {
+			return fmt.Errorf("failed to decrypt ciphertext with secretbox")
+		}
+
+		outputPath := strings.TrimSuffix(inputPath, ".kanuka")
+		outputPaths = append(outputPaths, outputPath)
+		// #nosec G306 -- We want the decrypted .env file to be editable by the user
+		if err := os.WriteFile(outputPath, plaintext, 0644); err != nil {
+			return fmt.Errorf("failed to write to %s: %w", outputPath, err)
+		}
+	}
+	log.Println("✅ All environment files in the project have been decrypted 🎉")
+	log.Printf("The following files were written: %s", FormatPaths(outputPaths))
 	return nil
 }
