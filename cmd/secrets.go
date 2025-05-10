ac9d4c0c0b3a3628410c5b803b91d3f80f35f8e0
Aaron Guo <89121695+PolarWolf314@users.noreply.github.com>
2025-05-10 23:40:08 +1200

    style: update helper message on CLI

--- cmd/secrets.go
+++ cmd/secrets.go
@@ -10,7 +10,7 @@ var (
 	SecretsCmd = &cobra.Command{
 		Use:   "secrets",
 		Short: "Manage secrets stored in the repository",
-		Long:  `Provides encryption, decryption, addition, removal, initialization, and purging of secrets.`,
+		Long:  `Provides encryption, decryption, registration, removal, initialization, and purging of secrets.`,
 	}
 )
 
