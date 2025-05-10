ef797664610d14ff185891facbeabffa64483ddb
Aaron Guo <89121695+PolarWolf314@users.noreply.github.com>
2025-05-10 23:32:26 +1200

    feat: add flags on register command

--- cmd/secrets_register.go
+++ cmd/secrets_register.go
@@ -5,6 +5,17 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var username string
+
+func init() {
+	registerCmd.Flags().BoolVarP(&verbose, "verbose", "v", false, "enable verbose output")
+	registerCmd.Flags().StringVarP(&username, "user", "u", "", "username to register for access")
+	if err := registerCmd.MarkFlagRequired("user"); err != nil {
+		printError("Failed to mark --user flag as required", err)
+		return
+	}
+}
+
 var registerCmd = &cobra.Command{
 	Use:   "register",
 	Short: "Registers a new user to be given access to the repository's secrets",
