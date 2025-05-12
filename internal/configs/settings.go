c5c396f9fd170aec5afa355297e5fac1743d6aee
Aaron Guo <89121695+PolarWolf314@users.noreply.github.com>
2025-05-12 17:32:47 +1200

    feat: create new settings file for shared paths and configs

---
+++ internal/configs/settings.go
@@ -0,0 +1,76 @@
+package configs
+
+import (
+	"kanuka/internal/utils"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+type UserSettings struct {
+	UserKeysPath    string
+	UserConfigsPath string
+	Username        string
+}
+
+type ProjectSettings struct {
+	ProjectUUID          string
+	ProjectName          string
+	ProjectPath          string
+	ProjectPublicKeyPath string
+	ProjectSecretsPath   string
+}
+
+var (
+	UserKanukaSettings    *UserSettings
+	ProjectKanukaSettings *ProjectSettings
+)
+
+func init() {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatalf("error getting home directory: %s", err)
+	}
+
+	configDir, err := os.UserConfigDir()
+	if err != nil {
+		log.Fatalf("error getting config directory: %s", err)
+	}
+
+	dataDir := os.Getenv("XDG_DATA_HOME")
+
+	if dataDir == "" {
+		dataDir = filepath.Join(homeDir, ".local", "share")
+	}
+
+	username, err := utils.GetUsername()
+	if err != nil {
+		log.Fatalf("error getting username: %s", err)
+	}
+
+	// This is independent of what repo you are in, so it is ok to init here
+	UserKanukaSettings = &UserSettings{
+		UserKeysPath:    filepath.Join(dataDir, "kanuka", "keys"),
+		UserConfigsPath: filepath.Join(configDir, "kanuka"),
+		Username:        username,
+	}
+}
+
+func InitProjectSettings() {
+	projectName, err := utils.GetProjectName()
+	if err != nil {
+		log.Fatalf("error getting project name: %s", err)
+	}
+
+	projectPath, err := utils.FindProjectKanukaRoot()
+	if err != nil {
+		log.Fatalf("error getting project root: %s", err)
+	}
+
+	ProjectKanukaSettings = &ProjectSettings{
+		ProjectName:          projectName,
+		ProjectPath:          projectPath,
+		ProjectPublicKeyPath: filepath.Join(projectPath, ".kanuka", "public_keys"),
+		ProjectSecretsPath:   filepath.Join(projectPath, ".kanuka", "secrets"),
+	}
+}
