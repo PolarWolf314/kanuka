12bab917e548d145e0d7512f709dcb900388aec0
Aaron Guo <89121695+PolarWolf314@users.noreply.github.com>
2025-05-12 22:10:51 +1200

    refactor: make decrypt method use settings

--- cmd/secrets_decrypt.go
+++ cmd/secrets_decrypt.go
@@ -1,9 +1,9 @@
 package cmd
 
 import (
+	"kanuka/internal/configs"
 	"kanuka/internal/secrets"
 	"kanuka/internal/utils"
-	"os"
 	"path/filepath"
 
 	"github.com/fatih/color"
@@ -12,6 +12,8 @@ import (
 
 func init() {
 	decryptCmd.Flags().BoolVarP(&verbose, "verbose", "v", false, "enable verbose output")
+
+	configs.InitProjectSettings()
 }
 
 var decryptCmd = &cobra.Command{
@@ -21,39 +23,32 @@ var decryptCmd = &cobra.Command{
 		spinner, cleanup := startSpinner("Decrypting environment files...", verbose)
 		defer cleanup()
 
-		projectRoot, err := utils.FindProjectKanukaRoot()
-		if err != nil {
-			printError("Failed to obtain project root", err)
-			return
-		}
-		if projectRoot == "" {
+		projectName := configs.ProjectKanukaSettings.ProjectName
+		projectPath := configs.ProjectKanukaSettings.ProjectPath
+
+		if projectPath == "" {
 			finalMessage := color.RedString("✗") + " Kanuka has not been initialized\n" +
 				color.CyanString("→") + " Please run " + color.YellowString("kanuka secrets init") + " instead\n"
 			spinner.FinalMSG = finalMessage
 			return
 		}
 
-		// Step 1: Check for .kanuka files
 		// TODO: In future, add config options to list which dirs to ignore. .kanuka/ ignored by default
-		listOfKanukaFiles, err := secrets.FindEnvOrKanukaFiles(projectRoot, []string{}, true)
+		listOfKanukaFiles, err := secrets.FindEnvOrKanukaFiles(projectPath, []string{}, true)
 		if err != nil {
 			printError("Failed to find environment files", err)
 			return
 		}
 		if len(listOfKanukaFiles) == 0 {
-			finalMessage := color.RedString("✗") + " No encrypted environment (" + color.YellowString(".kanuka") + ") files found in " + color.YellowString(projectRoot) + "\n"
+			finalMessage := color.RedString("✗") + " No encrypted environment (" + color.YellowString(".kanuka") + ") files found in " + color.YellowString(projectPath) + "\n"
 			spinner.FinalMSG = finalMessage
 			return
 		}
 
-		// Step 2: Get project's encrypted symmetric key
-		currentUsername, err := utils.GetUsername()
-		if err != nil {
-			printError("Failed to get username", err)
-			return
-		}
+		username := configs.UserKanukaSettings.Username
+		userKeysPath := configs.UserKanukaSettings.UserKeysPath
 
-		encryptedSymKey, err := secrets.GetProjectKanukaKey(currentUsername)
+		encryptedSymKey, err := secrets.GetProjectKanukaKey(username)
 		if err != nil {
 			finalMessage := color.RedString("✗") + " Failed to obtain your " +
 				color.YellowString(".kanuka") + " file. Are you sure you have access?\n" +
@@ -62,14 +57,7 @@ var decryptCmd = &cobra.Command{
 			return
 		}
 
-		homeDir, err := os.UserHomeDir()
-		if err != nil {
-			printError("Failed to get user's home directory", err)
-			return
-		}
-		projectName := filepath.Base(projectRoot)
-		privateKeyPath := filepath.Join(homeDir, ".config", ".kanuka", "keys", projectName)
-
+		privateKeyPath := filepath.Join(userKeysPath, projectName)
 		privateKey, err := secrets.LoadPrivateKey(privateKeyPath)
 		if err != nil {
 			finalMessage := color.RedString("✗") + " Failed to get your private key file. Are you sure you have access?\n" +
@@ -78,7 +66,6 @@ var decryptCmd = &cobra.Command{
 			return
 		}
 
-		// Step 3: Decrypt user's kanuka file (get symmetric key)
 		symKey, err := secrets.DecryptWithPrivateKey(encryptedSymKey, privateKey)
 		if err != nil {
 			finalMessage := color.RedString("✗") + " Failed to decrypt your " +
@@ -89,7 +76,6 @@ var decryptCmd = &cobra.Command{
 			return
 		}
 
-		// Step 4: Decrypt all .kanuka files
 		if err := secrets.DecryptFiles(symKey, listOfKanukaFiles, verbose); err != nil {
 			finalMessage := color.RedString("✗") + " Failed to decrypt the project's " +
 				color.YellowString(".kanuka") + " files. Are you sure you have access?\n" +
@@ -99,7 +85,7 @@ var decryptCmd = &cobra.Command{
 		}
 
 		// we can be sure they exist if the previous function ran without errors
-		listOfEnvFiles, err := secrets.FindEnvOrKanukaFiles(projectRoot, []string{}, false)
+		listOfEnvFiles, err := secrets.FindEnvOrKanukaFiles(projectPath, []string{}, false)
 		if err != nil {
 			printError("Failed to find environment files", err)
 			return
