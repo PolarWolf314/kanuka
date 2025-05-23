2750adbee655f8f4d1c5e33e83d7753bd7a36e5d
Aaron Guo <89121695+PolarWolf314@users.noreply.github.com>
2025-05-23 18:20:30 +1200

    docs: add custom theme

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -7,6 +7,7 @@ export default defineConfig({
   integrations: [
     starlight({
       title: "Kānuka",
+      customCss: ["./src/styles/custom.css"],
       social: [
         {
           icon: "github",
