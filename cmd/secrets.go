adce4000d26ac5bb77d0209757f1df1adba59b89
Aaron Guo <89121695+PolarWolf314@users.noreply.github.com>
2025-06-15 14:41:13 +1200

    feat: add persistent free run and logging

--- cmd/secrets.go
+++ cmd/secrets.go
@@ -1,20 +1,33 @@
 package cmd
 
 import (
+	logger "github.com/PolarWolf314/kanuka/internal/logging"
 	"github.com/spf13/cobra"
 )
 
 var (
 	verbose bool
+	debug   bool
+	Logger  logger.Logger
 
 	SecretsCmd = &cobra.Command{
 		Use:   "secrets",
 		Short: "Manage secrets stored in the repository",
 		Long:  `Provides encryption, decryption, registration, removal, initialization, and purging of secrets.`,
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			Logger = logger.Logger{
+				Verbose: verbose,
+				Debug:   debug,
+			}
+			Logger.Debugf("Initializing secrets command with verbose=%t, debug=%t", verbose, debug)
+		},
 	}
 )
 
 func init() {
+	SecretsCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "enable verbose output")
+	SecretsCmd.PersistentFlags().BoolVarP(&debug, "debug", "d", false, "enable debug output")
+
 	SecretsCmd.AddCommand(encryptCmd)
 	SecretsCmd.AddCommand(decryptCmd)
 	SecretsCmd.AddCommand(createCmd)
