be4275ed7dfb5f97a1a04944a522d5764689f1cf
Aaron Guo <89121695+PolarWolf314@users.noreply.github.com>
2025-06-15 15:32:13 +1200

    fix: remove spinner when in --debug

--- cmd/secrets_helper_methods.go
+++ cmd/secrets_helper_methods.go
@@ -12,13 +12,13 @@ import (
 
 func printError(message string, err error) {
 	Logger.Errorf("%s: %v", message, err)
-	if !verbose {
+	if !verbose && !debug {
 		log.SetOutput(os.Stdout)
 	}
 	log.Fatalf("❌ %s: %v", message, err)
 }
 
-// startSpinner creates and starts a spinner with the given message when not in verbose mode.
+// startSpinner creates and starts a spinner with the given message when not in verbose or debug mode.
 // Returns the spinner and a function that should be deferred to clean up.
 func startSpinner(message string, verbose bool) (*spinner.Spinner, func()) {
 	Logger.Debugf("Starting spinner with message: %s", message)
@@ -30,24 +30,24 @@ func startSpinner(message string, verbose bool) (*spinner.Spinner, func()) {
 		printError("Failed to create a spinner", err)
 	}
 
-	if !verbose || !debug {
+	if !verbose && !debug {
 		Logger.Debugf("Starting spinner in non-verbose mode")
 		s.Start()
 		// Ensure log output is discarded unless in verbose mode
 		log.SetOutput(io.Discard)
 	} else {
-		Logger.Infof("Running in verbose mode: %s", message)
+		Logger.Infof("Running in verbose or debug mode: %s", message)
 	}
 
 	cleanup := func() {
-		if !verbose || !debug {
+		if !verbose && !debug {
 			Logger.Debugf("Stopping spinner and restoring log output")
 			log.SetOutput(os.Stdout)
 			s.Stop()
 		} else {
 			// In verbose or debug mode, manually print the final message if it's set
 			if s.FinalMSG != "" {
-				Logger.Debugf("Displaying final message in verbose mode")
+				Logger.Debugf("Displaying final message in verbose/debug mode")
 				fmt.Print(s.FinalMSG)
 			}
 		}
